9f805b08adf8917ff9ef1352c2d8fc21f6c26bce
yayapipi <yayapipi@hotmail.com>
2020-12-16 13:12:46 +0800

    color bat

--- fullpush.bat
+++ fullpush.bat
@@ -6,19 +6,19 @@ git status
 git add .
 git commit -m "%commit%"
 git push -u origin master
-echo Public Commit Pushed
+echo ^<ESC^>[31m Public Commit Pushed
 cd ..
 git status
 git add .
 git rm --cached public -f
 git commit -m "%commit%"
 git push -u origin master
-echo Root Commit Pushed
+echo ^<ESC^>[31m Root Commit Pushed
 cd themes/hugo-PaperMod
 git status
 git add .
 git rm --cached public -f
 git commit -m "%commit%"
 git push -u origin main
-echo Themes Commit Pushed
+echo ^<ESC^>[31m Themes Commit Pushed
 pause
